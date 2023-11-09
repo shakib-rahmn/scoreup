4af9544484800d7e389f1dd81c1007d2eba3dd08
Shakib Rahman <98702414+shakib-rahmn@users.noreply.github.com>
2023-11-10 05:59:15 +0600

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import StudentTable from "../components/StudentTable";
-import EditForm from "../components/EditForm";
 
 const Dashboard = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<StudentTable />} />
-        <Route path="/:id" element={<EditForm />} />
-      </Routes>
+      <StudentTable />
     </>
   );
 };
